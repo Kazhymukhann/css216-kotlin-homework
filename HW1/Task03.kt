0ad5bdfe981ea9aa31cf4d66ca6e320bb66e59cd
Kazhymukhann <92291283+Kazhymukhann@users.noreply.github.com>
2026-09-24 15:23:13 +0500

    HW1: Task 3

---
+++ HW1/Task03.kt
@@ -0,0 +1,8 @@
+package hw1.task03
+
+fun main() {
+    val discountPercentage = 20
+    val item = "Google Chromecast"
+    val offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    println(offer)
+}
