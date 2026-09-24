4a0bd2089194b23b469026826d1b9eb512985b53
Kazhymukhann <92291283+Kazhymukhann@users.noreply.github.com>
2026-09-24 15:23:13 +0500

    HW1: Task 9

---
+++ HW1/Task09.kt
@@ -0,0 +1,11 @@
+package hw1.task09
+
+fun main() {
+    println(compareTime(300, 250))
+    println(compareTime(300, 300))
+    println(compareTime(200, 220))
+}
+
+fun compareTime(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    return timeSpentToday > timeSpentYesterday
+}
