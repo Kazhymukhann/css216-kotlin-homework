2bc27548612785c4a9dca59934188f82d4f5c16b
Kazhymukhann <92291283+Kazhymukhann@users.noreply.github.com>
2026-09-24 15:23:13 +0500

    HW1: Task 4

---
+++ HW1/Task04.kt
@@ -0,0 +1,8 @@
+package hw1.task04
+
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
